f67c7219ce0f428f8cc3fcbac9a2df599ad98e7d
Elis <199841193+Elis-2@users.noreply.github.com>
2025-02-19 16:59:01 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        basic.showIcon(IconNames.Yes)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(0)
+})
+basic.showIcon(IconNames.No)
+radio.setGroup(2)
+radio.sendString("")
 basic.forever(function () {
 	
 })
