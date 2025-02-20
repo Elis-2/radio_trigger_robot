a61a4da274354438325511a63eb1e88b7f17ec6f
Elis <199841193+Elis-2@users.noreply.github.com>
2025-02-20 09:18:16 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,12 +3,6 @@ radio.onReceivedNumber(function (receivedNumber) {
         basic.showIcon(IconNames.Yes)
     }
 })
-input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(0)
-})
-basic.showIcon(IconNames.No)
-radio.setGroup(2)
-radio.sendString("")
 input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
     radio.sendValue("left", 1)
@@ -18,17 +12,6 @@ input.onButtonPressed(Button.A, function () {
         # # # # #
         . # . . .
         . . . . .
-        `)
-})
-input.onButtonPressed(Button.AB, function () {
-    basic.clearScreen()
-    radio.sendValue("fwd", 1)
-    basic.showLeds(`
-        . . # . .
-        . # # # .
-        . . # . .
-        . . # . .
-        . . # . .
         `)
 })
 input.onButtonPressed(Button.B, function () {
@@ -46,10 +29,37 @@ input.onLogoEvent(TouchButtonEvent.Touched, function () {
     basic.clearScreen()
     radio.sendValue("servo", 1)
     basic.showLeds(`
-        # # # # #
+        # # # . .
         . . # . .
         # # # # #
         . # # # .
         . # # # .
         `)
+})
+basic.showIcon(IconNames.No)
+radio.setGroup(2)
+radio.sendString("")
+let conected = false
+let test = false
+let main = false
+input.onButtonPressed(Button.AB, function () {
+    if (!conected){
+        radio.sendNumber(0)
+    }else if (test){
+    basic.clearScreen()
+    radio.sendValue("fwd", 1)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    }else if (!test){
+        radio.sendNumber(2)
+        basic.showIcon(IconNames.Target)
+    }
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function() {
+    test = !test
 })
