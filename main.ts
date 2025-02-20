59a320e88411be1228e62044d0725af356db593f
Elis <199841193+Elis-2@users.noreply.github.com>
2025-02-20 09:31:09 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,40 +1,47 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         basic.showIcon(IconNames.Yes)
+        conected = true
     }
 })
 input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    radio.sendValue("left", 1)
-    basic.showLeds(`
+    if (test) {
+        basic.clearScreen()
+        radio.sendValue("left", 1)
+        basic.showLeds(`
         . . . . .
         . # . . .
         # # # # #
         . # . . .
         . . . . .
         `)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
-    radio.sendValue("right", 1)
-    basic.showLeds(`
+    if (test) {
+        basic.clearScreen()
+        radio.sendValue("right", 1)
+        basic.showLeds(`
         . . . . .
         . . . # .
         # # # # #
         . . . # .
         . . . . .
         `)
+    }
 })
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    basic.clearScreen()
-    radio.sendValue("servo", 1)
-    basic.showLeds(`
+    if (test) {
+        basic.clearScreen()
+        radio.sendValue("servo", 1)
+        basic.showLeds(`
         # # # . .
         . . # . .
         # # # # #
         . # # # .
         . # # # .
         `)
+    }
 })
 basic.showIcon(IconNames.No)
 radio.setGroup(2)
@@ -43,28 +50,31 @@ let conected = false
 let test = false
 let main = false
 input.onButtonPressed(Button.AB, function () {
-    if (!conected){
+    if (!conected) {
         radio.sendNumber(0)
-    }else if (test){
-    basic.clearScreen()
-    radio.sendValue("fwd", 1)
-    basic.showLeds(`
+        basic.showIcon(IconNames.Chessboard)
+    } else if (test) {
+        basic.clearScreen()
+        radio.sendValue("fwd", 1)
+        basic.showLeds(`
         . . # . .
         . # # # .
         . . # . .
         . . # . .
         . . # . .
         `)
-    }else if (!test){
+    } else if (!test) {
         radio.sendNumber(2)
         basic.showIcon(IconNames.Target)
     }
 })
-input.onLogoEvent(TouchButtonEvent.LongPressed, function() {
-    test = !test
-    if (test){
-        basic.showString("T")
-    }else{
-        basic.showString("E")
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    if (conected) {
+        test = !test
+        if (test) {
+            basic.showString("T")
+        } else {
+            basic.showString("E")
+        }
     }
 })
