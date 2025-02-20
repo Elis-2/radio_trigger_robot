f1e5a5b70a2e134632fd22551be0d59463ddb3fc
Elis <199841193+Elis-2@users.noreply.github.com>
2025-02-20 08:53:34 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,47 @@ input.onButtonPressed(Button.AB, function () {
 basic.showIcon(IconNames.No)
 radio.setGroup(2)
 radio.sendString("")
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    radio.sendValue("left", 1)
+    basic.showLeds(`
+        . . . . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+    radio.sendValue("fwd", 1)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    radio.sendValue("right", 1)
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . . . .
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    basic.clearScreen()
+    radio.sendValue("servo", 1)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        # # # # #
+        . # # # .
+        . # # # .
+        `)
 })
